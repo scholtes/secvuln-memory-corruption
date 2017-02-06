5c0647162ed967b7bcf8b6817baf9ce05c48be10
scholtes <scholtes.garrett@gmail.com>
2017-02-05 19:42:44 -0500

    Basic fix

--- ANewClass.cpp
+++ ANewClass.cpp
@@ -3,5 +3,5 @@
 #include <stdint.h>
 
 ANewClass::ANewClass(unsigned long long input) : ABaseClass((unsigned short)input) {
-  sprintf(this->get_buf(), "%llu", input);
+  sprintf(this->get_buf(), "%llu", input % 1000000);
 }
--- expl1.cpp
+++ expl1.cpp
@@ -5,7 +5,15 @@
 
 int
 main(int argc, char **argv) {
-   ABaseClass a = ANewClass(0x4040404040404040404040404040);
+
+   ABaseClass a = ANewClass(18446744073709551615);
+
+   // Look at some noteworthy addresses
+   printf("%p\n", (void *)&ANewClass::get_buf);
+   printf("%p\n", (void *)main);
+   printf("%p\n", &a);
+   printf("%p\n", a.get_buf());
 
    a.show_data();
+   printf("Still got here\n");
 }
