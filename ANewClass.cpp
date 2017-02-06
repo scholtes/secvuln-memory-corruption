c8138c998f2ddbc3566138698824065158cb6182
scholtes <scholtes.garrett@gmail.com>
2017-02-06 18:15:14 -0500

    Undo %hu and add expl1 demo changes

--- ANewClass.cpp
+++ ANewClass.cpp
@@ -3,5 +3,5 @@
 #include <stdint.h>
 
 ANewClass::ANewClass(unsigned long long input) : ABaseClass((unsigned short)input) {
-  sprintf(this->get_buf(), "%hu", input % 1000000);
+  sprintf(this->get_buf(), "%llu", input % 1000000);
 }
--- expl1.cpp
+++ expl1.cpp
@@ -6,7 +6,7 @@
 int
 main(int argc, char **argv) {
 
-   ABaseClass a = ANewClass(18446744073709551615);
+   ABaseClass a = ANewClass(100000000123456);
 
    // Look at some noteworthy addresses
    printf("%p\n", (void *)&ANewClass::get_buf);
@@ -15,5 +15,8 @@ main(int argc, char **argv) {
    printf("%p\n", a.get_buf());
 
    a.show_data();
+
+   // If you replace ANewClass.cpp with the provided lab code, 
+   // we will still get here, but will return with a segfault
    printf("Still got here\n");
 }
